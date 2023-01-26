822a22952f8169e31518615e8b203ea633eafc60
shubhampatil05 <shubhampatil3468@gmail.com>
2023-01-26 13:33:17 +0530

    todo

---
+++ index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import { App } from "./Components/11_rtk Todolist/App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+  </>
+);
